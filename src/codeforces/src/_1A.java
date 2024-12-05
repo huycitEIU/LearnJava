4a72a6c0512682ae6b749e40f12e087ab08bf1cc
Trần Gia Huy <huy.trangia.cit23@eiu.edu.vn>
2024-12-05 23:23:53 +0700

    Create _1A.java

---
+++ src/codeforces/src/_1A.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class _1A {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, m, a;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = sc.nextInt();
+        n = n / a + ((n % a > 0) ? 1 : 0);
+        m = m / a + ((m % a > 0) ? 1 : 0);
+        System.out.println(n * m);
+    }
+}
