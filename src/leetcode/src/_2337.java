40481bc1a568f581f23d8ebd79f73c39c2f121a1
Trần Gia Huy <huy.trangia.cit23@eiu.edu.vn>
2024-12-05 20:17:31 +0700

    add solution for problem 2337. Move Pieces to Obtain a String

---
+++ src/leetcode/src/_2337.java
@@ -0,0 +1,30 @@
+public class _2337 {
+    public static void main(String[] args) {
+        String str1 = "_L__R__R_";
+        String str2 = "L______RR";
+        Solution solution = new Solution();
+        System.out.println(solution.canChange(str1, str2));
+    }
+}
+class Solution {
+    public boolean canChange(String start, String target) {
+        int s = 0;
+        int t = 0;
+        int len = start.length();
+        while (s <= len && t <= len) {
+            
+            while (s < len && start.charAt(s) == '_') s++;
+            while (t < len && target.charAt(t) == '_') t++;
+            
+            if (s == len || t == len) return s == len && t == len;
+
+            if (start.charAt(s) != target.charAt(t)) return false;
+            
+            if (target.charAt(t) == 'L' && s < t) return false;
+            if (target.charAt(t) == 'R' && s > t) return false;
+            s++;
+            t++;
+        }      
+        return true;
+    }
+}
