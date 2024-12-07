e03b4fb40910f68e74bb1298f7b4f3f4772af570
Trần Gia Huy <huy.trangia.cit23@eiu.edu.vn>
2024-12-07 16:44:19 +0700

    change solution

--- src/codeforces/src/_50A.java
+++ src/codeforces/src/_50A.java
@@ -1,8 +1,6 @@
 public class _50A {
+    static java.util.Scanner sc = new java.util.Scanner(System.in);
     public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        System.out.println(m * n / 2);
+        System.out.println(sc.nextInt() * sc.nextInt() / 2);
     }
 }
